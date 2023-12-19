93f5451ac19493e6e3cdce3594425e77f9212030
asyasuri1991 <asyasuri@gmail.com>
2023-12-19 15:29:56 +0300

    добавила первый файл

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <ul>
+        <li><a href="">задание 1</a></li>
+        <li><a href="">задание 2-1</a></li>
+        <li><a href="">задание 2-2</a></li>
+    </ul>
+</body>
+</html>
